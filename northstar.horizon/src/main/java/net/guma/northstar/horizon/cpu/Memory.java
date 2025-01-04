a2379054dc8e239af55211c9421aa430906796ac
Alexis Guma <agtghz@guma.net>
2025-01-04 09:45:42 -0500

    Update copyright to 2025

--- northstar.horizon/src/main/java/net/guma/northstar/horizon/cpu/Memory.java
+++ northstar.horizon/src/main/java/net/guma/northstar/horizon/cpu/Memory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 Alexis Guma
+ * Copyright 2025 Alexis Guma
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
